3745cbf243e4421ee6b95daf905f8c5570097855
davidandrus <davidandrus.com@gmail.com>
2017-01-15 06:48:35 -0800

    feat: adding resume data to resumeBuilder

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,59 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  name: 'David Andrus',
+  role: 'Front End Engineer',
+  contacts : {
+      mobile: '555.555.5555',
+      email: 'someemail@someemail.com',
+      github: 'https://github.com/davidandrus',
+      // twitter: string (optional)
+      location: 'Seattle',
+  },
+  welcomeMessage: 'Hi, I am Dave, welcome to my resume',
+  skills: ['HTML', 'JavaScript', 'CSS'],
+  biopic: 'https://avatars0.githubusercontent.com/u/1655282?v=3&s=460',
+  display: function() {
+
+  }
+};
+
+var education = {
+  schools: [{
+    name: 'School 1',
+    location: 'Space',
+    degree: 'Web Design',
+    majors: ['Web Design'],
+    dates: ['2005-2006'],
+    url: 'http://someschool.com',
+  }],
+  onlineCourses: [{
+    title: 'Online Course',
+    school: 'Online Course',
+    dates: ['2005-2006'],
+    url: 'http://someschool.com',
+  }],
+  display: function() {
+
+  },
+};
+
+var work = {
+  jobs: [{
+    employer: 'Employer',
+    title: 'Front-end Developer',
+    location: 'Space',
+    dates: 'in progress',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis sed massa at convallis. Mauris at risus sit amet turpis fringilla pretium. Donec id vehicula tortor. In vulputate sem at odio eleifend, sed lobortis justo commodo. Etiam tempor luctus sapien quis aliquam. In malesuada sapien at dignissim facilisis. Donec dictum consectetur massa, ut pulvinar leo dictum quis.',
+  }],
+  display: function() {
+  }
+}
+
+var projects = {
+  projects: [{
+    title: 'Project 1',
+    dates: '2016-2017',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis sed massa at convallis. Mauris at risus sit amet turpis fringilla pretium. Donec id vehicula tortor. In vulputate sem at odio eleifend, sed lobortis justo commodo. Etiam tempor luctus sapien quis aliquam. In malesuada sapien at dignissim facilisis. Donec dictum consectetur massa, ut pulvinar leo dictum quis.',
+    images: ['<img src="http://placehold.it/300/200">']
+  }],
+  display: function() {}
+};
